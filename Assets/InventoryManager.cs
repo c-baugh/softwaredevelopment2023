62a2f78e0655eb2ec2a7ac815dea6ef062a867d1
Pryce <85907457+PryceWayne@users.noreply.github.com>
2024-03-07 03:26:41 -0500

    Added inventory, items and related classes
    Added new inventory items (Gold, Apple, Sword, etc.) to the project.
    Implemented item classes (itemClass, ConsumableClass, ToolClass, MiscClass) for better organization and functionality.
    Updated the InventoryManager and related scripts to handle the new items.
    Imported additional item textures from the Voxel Pack.

---
+++ Assets/InventoryManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public ItemClass itemToAdd;
+    public ItemClass itemToRemove;
+    public List<ItemClass> items = new List<ItemClass>();
+
+    public void Start()
+    {
+        Add(itemToAdd);
+        Remove(itemToRemove);
+    }
+    public void Add(ItemClass item)
+    {
+        items.Add(item);
+    }
+
+    public void Remove(ItemClass item)
+    {
+
+        items.Remove(item);
+
+    }
+}
+
+
+
