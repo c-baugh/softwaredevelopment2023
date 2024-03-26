466f78feadce3ff76b829fecda0032ce2b0b2716
Pryce <85907457+PryceWayne@users.noreply.github.com>
2024-03-26 02:42:10 -0400

    Inventory Management System Edit + Add Item Stack Handling
    - Updated the InventoryManager script to handle item stacks and display item quantities in UI.
    - Modified the RefreshUI method to properly display item icons and quantities.
    - Implemented stackable item handling in the Add method to increase the quantity if the item already exists in inventory.
    - Added checks to ensure that the inventory does not exceed the slot limit when adding new items.
    - Revised the Remove method to properly handle item removal and adjust quantities accordingly.

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class Inventory : MonoBehaviour
@@ -8,7 +9,7 @@ public class Inventory : MonoBehaviour
     [SerializeField] private ItemClass itemToAdd;
     [SerializeField] private ItemClass itemToRemove;
 
-    public List<ItemClass> items = new List<ItemClass>();
+    public List<SlotClass> items = new List<SlotClass>();
 
     private GameObject[] slots; 
     public void Start()
@@ -32,29 +33,86 @@ public class Inventory : MonoBehaviour
             try
             {
                 slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
-                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].itemIcon;
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].GetItem().itemIcon;
+                if (items[i].GetItem().isStackable)
+                    slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = items[i].GetQuantity() + "";
+                else
+                    slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "";
             }
 
             catch
             {
                 slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
                 slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+                slots[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "";
+                 
 
             }
         }   
     }
 
-    public void Add(ItemClass item)
+    public bool Add(ItemClass item)
     {
-        items.Add(item);
-        RefreshUI(); 
+        // items.Add(item);
+        //check if inventory contains item
+        
+
+        SlotClass slot = Contains(item);
+        if (slot != null && slot.GetItem().isStackable)
+            slot.AddQuantity(1);
+        else
+        {
+            if (items.Count < slots.Length)
+                items.Add(new SlotClass(item, 1));
+            else
+                return false;
+        }
+            
+        RefreshUI();
+        return true;
     }
 
-    public void Remove(ItemClass item)
+    public bool Remove(ItemClass item)
     {
 
-        items.Remove(item);
+        // items.Remove(item);
+        SlotClass temp = Contains(item);
+        if (temp != null)
+        {
+            if (temp.GetQuantity() > 1)
+                temp.SubQuantity(1);
+            else
+            {
+                SlotClass slotToRemove = new SlotClass();
+
+                foreach (SlotClass slot in items)
+                {
+                    if (slot.GetItem() == item)
+                    {
+                        slotToRemove = slot;
+                        break;
+                    }
+                }
+
+                items.Remove(slotToRemove);
+            }
+        }
+        else
+        {
+            return false; 
+        }
         RefreshUI();
+        return true; 
+
+    }
+    public SlotClass Contains(ItemClass item)
+    {
+        foreach (SlotClass slot in items)
+        {
+            if (slot.GetItem() == item)
+                return slot;
+        }
+            return null;
     }
 }
 
--- Assets/itemClass.cs
+++ Assets/itemClass.cs
@@ -7,7 +7,7 @@ public abstract class ItemClass : ScriptableObject
     [Header("Item")] //data shared across all item
     public string itemName;
     public Sprite itemIcon;
-
+    public bool isStackable = true;
     public abstract ItemClass GetItem();
     public abstract ToolClass GetTool();
     public abstract MiscClass GetMisc();
