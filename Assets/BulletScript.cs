63b8d1a59ec6a599eef7836e8ac9b297758f0efa
c-baugh <crbaugh@eagles.usi.edu>
2024-04-15 22:27:03 -0500

    Added Damage and Collisions to Blade Shoot Ability

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float force;
+    private int damage = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mainCam = GameObject.FindGameObjectWithTag("Main Camera").GetComponent<Camera>();
+        mainCam = GameObject.FindObjectOfType<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(rotation.x, rotation.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.GetComponent<Health>() != null)
+        {
+            Health health = collider.GetComponent<Health>();
+            health.Damage(damage);
+        }
+    }
+
+}
